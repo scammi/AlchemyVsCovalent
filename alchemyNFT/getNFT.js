2f475d4ae50014c4668c15063d4d4642607ba64e
scammi <scammi@gmail.com>
2022-02-16 09:05:08 -0300

    Update

--- alchemyNFT/getNFT.js
+++ alchemyNFT/getNFT.js
@@ -10,9 +10,9 @@ export default function getNFT() {
 
   // Replace with your Alchemy API key:
   const apiKey = process.env.apiKeyAlchemy;
-  const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${apiKey}/getNFTs/`;
+  const baseURL = `https://eth-kovan.g.alchemy.com/v2/${apiKey}/getNFTs/`;
   // Replace with the wallet address you want to query:
-  const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+  const ownerAddr = "0x7856C7F4A4Fa0a65Ed2950FbAcFEf7ACB17555E6";
   const fetchURL = `${baseURL}?owner=${ownerAddr}`;
   
   // Make the request and print the formatted response:
--- alchemyNFT/getNFTMetadata.js
+++ alchemyNFT/getNFTMetadata.js
@@ -8,9 +8,9 @@ export default function getNFTMetadata() {
   };
   
   const apiKey = process.env.apiKeyAlchemy;
-  const baseURL = `https://polygon-mainnet.g.alchemy.com/v2/${apiKey}/getNFTMetadata`;
-  const contractAddr = "0xe2a9b15E283456894246499Fb912CCe717f83319";
-  const tokenId = "240";
+  const baseURL = `https://eth-kovan.g.alchemy.com/v2/${apiKey}/getNFTMetadata`;
+  const contractAddr = "0xD4F7389297d9cea850777EA6ccBD7Db5817a12b2";
+  const tokenId = "22";
   const fetchURL = `${baseURL}?contractAddress=${contractAddr}&tokenId=${tokenId}`;
   
   const metadata = fetch(fetchURL, requestOptions)
